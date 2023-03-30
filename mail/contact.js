041f4bc907fd644db75c9f050cedb54513c2139f
laukic1 <dlauko96@gmail.com>
2023-03-31 00:40:21 +0200

    ss

--- mail/contact.js
+++ mail/contact.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+    $("#contactForm input").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
         },
